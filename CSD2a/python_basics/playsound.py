f53518fbb19e0931007dc00d57f6009389727e24
Sydeline <straver.joeri@gmail.com>
2024-09-06 12:02:17 +0200

    Added user interaction repetition function

--- CSD2a/python_basics/playsound.py
+++ CSD2a/python_basics/playsound.py
@@ -1,14 +1,17 @@
 import pygame
 import time
 
+#intializing mixer
 pygame.mixer.init()
 
+#asking for sound repetition value
+times = int(input("Please enter amount of times to play the sound: \n"))
+
+#loading sound file
 soundfile = pygame.mixer.Sound("sounds/kick.wav")
 
-soundfile.play()
-print("Sound played")
-
-for x in range(10):
+#playing sound for asked times
+for x in range(times):
     soundfile.play()
     print("Sound played")
     time.sleep(1)
