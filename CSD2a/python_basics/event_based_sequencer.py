9dcd0f2bf37d4d0a980dc3a0770cd77dddc33bba
Sydeline <straver.joeri@gmail.com>
2024-10-08 15:21:41 +0200

    made changes to event_based_sequencer assignment

--- CSD2a/python_basics/event_based_sequencer.py
+++ CSD2a/python_basics/event_based_sequencer.py
@@ -7,56 +7,91 @@ note_16th_duration = 60 / BPM / 4
 #initializing sound and player
 pygame.mixer.init()
 
-kick = pygame.mixer.Sound("sounds/kick.wav")
-snare = pygame.mixer.Sound("sounds/snare.wav")
-hihat = pygame.mixer.Sound("sounds/hihat.wav")
+kick_sound = pygame.mixer.Sound("sounds/kick.wav")
+snare_sound = pygame.mixer.Sound("sounds/snare.wav")
+hihat_sound = pygame.mixer.Sound("sounds/hihat.wav")
 
+
+#events
 kick = {
     'timestamps': [], 
     'instrumentname': "kick", 
-    'instrument': kick,
+    'instrument': kick_sound,
     'sequence': [1,0,0,0, 1,0,0,0,  1,0,0,0,  1,0,0,0],
     'durations': []
 }
 snare = {
     'timestamps': [], 
     'instrumentname': "snare", 
-    'instrument': snare,
+    'instrument': snare_sound,
     'sequence': [0,0,0,0,  1,0,0,0,  0,0,0,0,  1,0,0,0],
     'durations': []
 }
 hihat = {
     'timestamps': [], 
     'instrumentname': "hihat", 
-    'instrument': hihat,
-    'sequence': [1,0,1,0,  1,0,1,0,  1,0,1,0,  1,0,1,0],
+    'instrument': hihat_sound,
+    'sequence': [1,0,1,0,  1,0,1,0,  1,0,1,0,  1,0,1,1],
     'durations': []
 }
 
 
-def create_timestamp(ts, sample):
-    return {'ts': ts, 'sample': sample}
+#dictionary in list function
+def create_timestamp(ts, sample, name):
+    return {'ts': ts, 'sample': sample, 'name': name}
 
+
+#transforming binary sequence in to usable timestamps
 events = []
-
-def binary_to_timestamps(instrument):
+def binary_to_timestamps(instrument, i):
     if instrument['sequence'][i] == 1:
-        events.append(create_timestamp(i*note_16th_duration, instrument['instrumentname']))
+        events.append(create_timestamp(i*note_16th_duration, instrument['instrument'], instrument['instrumentname']))
 
+
+#utilising function
 for i in range(16):
-    binary_to_timestamps(kick)
-    binary_to_timestamps(snare)
-    binary_to_timestamps(hihat)
+    binary_to_timestamps(kick, i)
+    binary_to_timestamps(snare, i)
+    binary_to_timestamps(hihat, i)
 
+
+#handling event 
 def handle_note_event(event):
-    print(event['instrumentname'])
-    event['instrument'].play()
+    print(event['name'])
+    event['sample'].play()
+
+events_temp = events
+
+#playback
+while True:
+    events = events_temp[:]
+    time_start = time.time()
+
+    while events:
+        event = events[0]
+        time_now = time.time() - time_start
+
+        if time_now >= event['ts']:
+            if events:
+                handle_note_event(events.pop(0))
+            else:
+                break
+        time.sleep(0.01)
+    
+    time.sleep(note_16th_duration)
+    
+
+
+
+# event = events.pop(0)
+# handle_note_event(event)
+# time.sleep(1)
+
+
+
+
 
 #functie voor spelen juiste smaple of juist moment
-
-
-
-print(events)
 
 
 
