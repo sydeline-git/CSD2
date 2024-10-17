3aed60b3e323572840d34ebc71473ece84a05822
Sydeline <straver.joeri@gmail.com>
2024-10-17 15:09:09 +0200

    fixed overlapping sound issues and added a slicer function

--- CSD2a/eindopdracht/eindopdracht.py
+++ CSD2a/eindopdracht/eindopdracht.py
@@ -5,10 +5,16 @@ import random
 
 BPM = 120
 note_16th_duration = 60 / BPM / 4
+slice_length = 16
 
 
 #initializing sound and player
 pygame.mixer.init()
+pygame.mixer.set_num_channels(3)
+
+kick_channel = pygame.mixer.Channel(0)
+snare_channel = pygame.mixer.Channel(1)
+hihat_channel = pygame.mixer.Channel(2)
 
 kick_sound = pygame.mixer.Sound("sounds/kick.wav")
 snare_sound = pygame.mixer.Sound("sounds/snare.wav")
@@ -20,6 +26,7 @@ kick = {
     'timestamps': [], 
     'instrumentname': "kick", 
     'instrument': kick_sound,
+    'channel': kick_channel,
     'sequence': [],
     'durations': []
 }
@@ -27,6 +34,7 @@ snare = {
     'timestamps': [], 
     'instrumentname': "snare", 
     'instrument': snare_sound,
+    'channel': snare_channel,
     'sequence': [],
     'durations': []
 }
@@ -34,6 +42,7 @@ hihat = {
     'timestamps': [], 
     'instrumentname': "hihat", 
     'instrument': hihat_sound,
+    'channel': hihat_channel,
     'sequence': [],
     'durations': []
 }
@@ -50,6 +59,16 @@ def text_to_binary():
 binary_list = text_to_binary()
 sequence_length = len(binary_list)//3
 
+def slicer(slice_length, list):
+    sliced_sequence = []
+
+    slice_start = random.randrange(len(list) - slice_length)
+
+    for _ in range(slice_length):
+        sliced_sequence.append(list.pop(slice_start))
+
+    return sliced_sequence
+
 #text to sequence
 def generate_text_sequence(instrument):
     temp_sequence = []
@@ -58,20 +77,9 @@ def generate_text_sequence(instrument):
         if binary_list:
             temp_sequence.append(binary_list.pop(0))
 
-    instrument['sequence'].extend(temp_sequence)
-    print(instrument['instrumentname'], instrument['sequence'])
+    sliced_sequence = slicer(slice_length, temp_sequence)
 
-
-
-#------ RANDOM BINARY ------#
-#generate random 1 or 0
-def random_binary():
-    return int(random.random() >= 0.5)
-
-#create random sequence
-def generate_binary_sequence(instrument, length):
-    temp_sequence = [random_binary() for _ in range(length)]
-    instrument['sequence'].extend(temp_sequence)
+    instrument['sequence'].extend(sliced_sequence)
     print(instrument['instrumentname'], instrument['sequence'])
 
 
@@ -80,38 +88,36 @@ def generate_binary_sequence(instrument, length):
 generate_text_sequence(kick)
 generate_text_sequence(snare)
 generate_text_sequence(hihat)
-# generate_binary_sequence(kick, sequence_length)
-# generate_binary_sequence(snare, sequence_length)
-# generate_binary_sequence(hihat, sequence_length)
 
 
 
 #------ SEQUENCE TO TIMESTAMPS ------#
 #dictionary in list function
-def create_timestamp(ts, sample, name):
-    return {'ts': ts, 'sample': sample, 'name': name}
+def create_timestamp(ts, sample, name, channel):
+    return {'ts': ts, 'sample': sample, 'name': name, 'channel': channel}
 
 #transforming binary sequence in to usable timestamps
 events = []
 def binary_to_timestamps(instrument, i):
     if instrument['sequence'][i] == 1:
-        events.append(create_timestamp(i*note_16th_duration, instrument['instrument'], instrument['instrumentname']))
+        events.append(create_timestamp(i*note_16th_duration, instrument['instrument'], instrument['instrumentname'], instrument['channel']))
 
 #utilising function
-for i in range(sequence_length):
+for i in range(slice_length):
     binary_to_timestamps(kick, i)
     binary_to_timestamps(snare, i)
     binary_to_timestamps(hihat, i)
 
 #handling event 
 def handle_note_event(event):
-    print(event['name'])
-    event['sample'].play()
+    # print(f"Playing: {event['name']} at {event['ts']}")
+    event['channel'].play(event['sample'])
 
 
 
 #------ PLAYBACK ------#
 events_temp = events
+
 
 #playback
 while True:
@@ -127,10 +133,28 @@ while True:
                 handle_note_event(events.pop(0))
             else:
                 break
-        time.sleep(0.01)
+        time.sleep(0.001)
     
     time.sleep(note_16th_duration)
     
+
+# generate_binary_sequence(kick, sequence_length)
+# generate_binary_sequence(snare, sequence_length)
+# generate_binary_sequence(hihat, sequence_length)
+
+
+# #------ RANDOM BINARY ------#
+# #generate random 1 or 0
+# def random_binary():
+#     return int(random.random() >= 0.5)
+
+# #create random sequence
+# def generate_binary_sequence(instrument, length):
+#     temp_sequence = [random_binary() for _ in range(length)]
+#     instrument['sequence'].extend(temp_sequence)
+#     print(instrument['instrumentname'], instrument['sequence'])
+
+
 
 
 
